adc7cd0e395340129da03f21ae200a79e8032182
uniicoorni <carolinekostina.gmail.com>
2024-03-20 11:33:25 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,161 +1,185 @@
-import { useRef } from "react";
+import React, {RefObject, useRef} from "react";
 import "./App.css";
-export const App = () => {
-  const inputRef = useRef<HTMLInputElement | null>(null);
+import { Layout, Button, Modal  } from 'antd';
 
-  function checkValid() {
-    if (!inputRef.current || !inputRef.current?.textContent) return;
-    const logicConstruction: string = inputRef.current?.textContent;
-    inputRef.current.innerHTML = inputRef.current?.textContent;
+class InputEngine {
+    element: RefObject<HTMLInputElement>
+    constructor(element: RefObject<HTMLInputElement>) {
+        this.element = element
 
-    const input =
-      '("Каролина Тони" OR ( ("Тони и Каролина" AND "Тони и Каролина" ) AND ("Тони и Каролина" AND "Тони и Каролина" ))) AND Каролина';
+    }
 
-    let words = [];
+    public parseSyntax(arrInput: string[]) {
+        const stack: string[] = [];
+        const output: string[] = [];
+        for (let i = 0; i < arrInput.length; i++) {
+            if (arrInput[i] === "(" || arrInput[i] === ")") {
+                stack.push(arrInput[i]);
+            }
+            const lengthStackOpen = stack.filter((el) => el === "(").length;
+            const lengthStackClose = stack.filter((el) => el === ")").length;
+            if (lengthStackOpen % 2 !== lengthStackClose % 2) {
+                output.push(`<span class="red">${arrInput[i]}</span>`);
+                stack.pop();
+                continue;
+            }
 
-    function Parser(input) {
-      const basicInput = input;
-      const bracketExp = /("(?:[^"\\]|\\.)*"|(?![AND]|[OR])([а-яА-Яa-zA-Z]+))/g;
-      words = input.match(bracketExp);
-      words.forEach((el, i) => {
-        input = input.replace(el, "#");
-      });
-      input = input
-        .replaceAll(" ", "")
-        .replaceAll("AND", "*")
-        .replaceAll("OR", "|");
-      input = input.split("");
-
-      if (input.includes('"')) {
-        throw new Error(`В строке не хватает кавычки`);
-      }
-
-      let start = 0;
-      let end = 0;
-      let prev = "";
-      let current = "";
-      const isValidate = true;
-      for (let i = 0; i < input.length; i++) {
-        if (input[i] === "(") {
-          start++;
-        } else if (input[i] === ")") {
-          end++;
-        } else {
-          current = input[i];
-          if (
-            ["|", "*"].indexOf(current) >= 0 &&
-            ["|", "*"].indexOf(prev) >= 0
-          ) {
-            throw new Error("Ошибка логических операторов");
-          }
-          if (["#"].indexOf(current) >= 0 && ["#"].indexOf(prev) >= 0) {
-            throw new Error("Ошибка ввода текста");
-          }
-          prev = current;
+            if (arrInput[i] === "(" || arrInput[i] === ")") {
+                output.push(`<span class="green">${arrInput[i]}</span>`);
+                continue;
+            }
+            if (arrInput[i] === "OR" || arrInput[i] === "AND") {
+                output.push(`<span class="orange">${arrInput[i]}</span>`);
+                continue;
+            }
+            output.push(`<span class="blue">${arrInput[i]}</span>`);
         }
-      }
-
-      if (start < end) {
-        throw new Error(`В строке не хватает открывающейся скобки`);
-      } else if (start > end) {
-        throw new Error(`В строке не хватает закрывающейся скобки`);
-      }
+        return output
     }
-    const start = performance.now();
 
-    for (let i = 0; i < 1e6; i++) {
-      Parser(input);
+    public validate(basicInput: string) {
+        let input: string | string[] = basicInput
+        const bracketExp = /("(?:[^"\\]|\\.)*"|(?![AND]|[OR])([а-яА-Яa-zA-Z]+))/g
+        const words  = input.match(bracketExp) as string[]
+        words.forEach((el) => {
+            if (typeof input === 'string') {
+                input = input.replace(el, '#')
+            }
+        })
+        input = input.replaceAll(/ /g, '').replaceAll('AND', '*').replaceAll('OR', '|').replaceAll('&nbsp;', '')
+        input = input.split('').filter(el => el.charCodeAt(0) !== 160)
+
+        if (input.includes('"')) {
+            return {
+                status: false,
+                detail: `В строке не хватает кавычки`,
+            }
+        }
+
+        let start = 0
+        let end = 0
+        let prev = ''
+        let current = ''
+        let response = {
+            status: true,
+            detail: `Вы молодец!`,
+        }
+        console.log(input.join(''))
+        charParser: for (let i = 0; i< input.length; i++) {
+            if (input[i] === '(') {
+                start ++
+            } else if (input[i] === ')') {
+                end ++
+            } else {
+                current = input[i]
+                if (['#'].indexOf(current) >= 0 && ['#'].indexOf(prev) >= 0) {
+                    response =  {
+                        status: false,
+                        detail: 'У вас ошибка в запросе',
+                    }
+                    break charParser
+                }
+                if (['|', '*'].indexOf(current) >= 0 && ['|', '*'].indexOf(prev) >= 0) {
+                    response =  {
+                        status: false,
+                        detail: 'Ошибка логических операторов',
+                    }
+                    break charParser
+                }
+
+                prev = current
+            }
+        }
+        if (!response.status) return  response
+
+        if (start < end ) {
+
+            return {
+                status: false,
+                detail: `В строке не хватает открывающейся скобки`,
+            }
+
+        } else if (start > end) {
+            return {
+                status: false,
+                detail: `В строке не хватает закрывающейся скобки`,
+            }
+        }
+
+        return response
+
     }
-    console.log(performance.now() - start);
+}
+export const App = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [input, setInput] = React.useState('')
+  const [Input, setInputEngine] = React.useState<InputEngine>()
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState('')
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+  const checkValid = (input: string) =>  {
+    if (Input === undefined) return 0
+
+    const logicConstruction = input;
     const separator = /(\(|\)|OR|AND|\[|\])/g;
-    // проверка на OR AND и скобки
-
-    // const sepForReg = /^(\([a-zA-Z]+(?:\s[a-zA-Z]+)?\)\s(OR|AND)\s\([a-zA-Z]+(?:\s[a-zA-Z]+)?\))$/
-    const testAndOr = /\b(OR|AND)\b/i;
-
-    const quotes = /("(?:[^"\\]|\\.)*"|(?![AND]|[OR])([а-яА-Яa-zA-Z]+))/g;
-
-    const bracketCheck =
-      /^(\([^()]*\)|[^()]+)(\s+(AND|OR)\s+(\([^()]*\)|[^()]+))*$/i;
     const arrInput = logicConstruction.split(separator);
-    console.log(arrInput);
-    const matches: string[] | null = logicConstruction.match(quotes);
-    console.log("MATCH", matches);
-    const output: string[] = [];
-    const stack: string[] = [];
+    const output: string[] = Input.parseSyntax(arrInput);
 
-    const arrString = logicConstruction.split("AND");
-    console.log(arrString);
+    const response = Input.validate(input);
+    if (!response.status) {
+        showModal()
+        setErrorMessage(response.detail)
 
-    for (let i = 0; i < arrInput.length; i++) {
-      if (arrInput[i] === "(" || arrInput[i] === ")") {
-        stack.push(arrInput[i]);
-      }
-      const lengthStackOpen = stack.filter((el) => el === "(").length;
-      const lengthStackClose = stack.filter((el) => el === ")").length;
-      if (lengthStackOpen % 2 !== lengthStackClose % 2) {
-        output.push(`<span class="red">${arrInput[i]}</span>`);
-        stack.pop();
-        continue;
-      }
-
-      if (arrInput[i] === "(" || arrInput[i] === ")") {
-        output.push(`<span class="green">${arrInput[i]}</span>`);
-        continue;
-      }
-      if (arrInput[i] === "OR" || arrInput[i] === "AND") {
-        output.push(`<span class="orange">${arrInput[i]}</span>`);
-        continue;
-      }
-      output.push(`<span class="blue">${arrInput[i]}</span>`);
     }
-
-    inputRef.current.innerHTML = output.join("");
-    //  arrInput.map((el) => {
-    //     if(el === '(' || el === ')') {
-    //         return `<span class="green">${el}</span>`
-    //     }
-    //     if(el === 'OR' || el === 'AND') {
-    //         return `<span class="orange">${el}</span>`
-    //     }
-    //     return `<span class="blue">${el}</span>`
-    // }).join('')
-
-    // if(!matches) {
-    //     return
-    // }
-
-    if (!testAndOr.test(inputRef.current.textContent)) {
-      console.log(inputRef.current?.textContent);
-      alert("Отсутвует логический оператор");
-      return;
-    }
-
-    // if (!quotes.test(inputRef.current.textContent)) {
-    //   console.log(inputRef.current?.textContent);
-    //   alert("ОШИБКА, отстутсвует кавычка");
-    //   return;
-    // }
-
-    if (!bracketCheck.test(inputRef.current.textContent)) {
-      console.log(inputRef.current?.textContent);
-      alert("Отсутвует скобка");
-      return;
-    }
-
-    // if(!sepForReg.test(inputRef.current.textContent)) {
-    //     console.log(inputRef.current?.textContent)
-    //     alert('Некорректное выражение')
-    //     return;
-    // }
+    (inputRef.current as HTMLDivElement).innerHTML = output.join("");
   }
 
-  return (
-    <>
-      <div contentEditable ref={inputRef} />
+  React.useEffect(() => {
+      setInputEngine( new InputEngine(inputRef) as InputEngine)
+  },[])
 
-      <button onClick={checkValid}>Проверить</button>
-    </>
+
+  return (
+    <Layout style={{
+        width: '80vw',
+        height: 400,
+        backgroundColor: 'transparent',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        gap: 10
+
+    }}>
+        <Layout
+            className='ant-input ant-input-outlined'
+            style={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'row'
+            }}
+            contentEditable
+            onInput={({target: {textContent}}: React.ChangeEvent<HTMLDivElement>) => setInput(textContent as string)}
+            ref={inputRef} />
+
+        <Button type="primary" size='large' style={{width: '50%'}} onClick={() => checkValid(input)}>Проверить</Button >
+
+        <Modal title="Ошибка синтаксиса!" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            <p>{errorMessage}</p>
+        </Modal>
+    </Layout>
   );
 };
 export default App;
